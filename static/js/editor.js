fa12e0fca955c5ed71f1a66bca0baa4de9b17047
li ling <lling.tan00@gmail.com>
2023-07-17 19:08:32 +0800

    Added editor

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,3 +1,8 @@
+// session_id placeholder
+var sessionId = '';
+var editor;
+var newSessionId = '';
+
 //setting up editor and passing data
 window.onload = function() {
     console.log("Setting up the editor...");  // This should show in the console when the page loads
@@ -5,9 +10,6 @@ window.onload = function() {
         lineNumbers: true,
         mode: "application/json"
     });
-
-    // session_id placeholder
-    var sessionId = '';
 
     // retrieve the data from browser localStorage
     var myData = JSON.parse(localStorage.getItem('myData'));
@@ -26,16 +28,36 @@ window.onload = function() {
             return objCopy;
         });
 
-
         // convert the data to a formatted string
         var dataAsString = JSON.stringify(myDataCopy, null, 2);
 
         //set the editor's value
         editor.getDoc().setValue(dataAsString);
+
+        //behavior after clicking the submit button
+        document.getElementById('submit-button').addEventListener('click',function(){
+            console.log("Submit button is clicked on editor")
+
+            // get the edited data
+            var currentEditorValue = editor.getValue();
+            try{
+                // parse it as JSON
+                var currentData = JSON.parse(currentEditorValue);
+                //add sessionId back
+                var updatedData = currentData.map(function(obj){
+                    var newObj = {"Session_ID": uuidv4(), ...obj}; //create a copy
+                    return newObj;
+                });
+                // convert the updated data back to a string
+                var updatedDataAsString = JSON.stringify(updatedData, null, 2);
+                console.log(updatedDataAsString);
+
+                //TODO: do a POST query to the database  
+            }catch(e){
+                console.error("The edited data is not in valid JSON, check the formatting: ", e);
+            }
+        })
     } else {
         console.log("No data found in localStorage");
     }
 };
-
-
-//behavior after clicking the submit button
